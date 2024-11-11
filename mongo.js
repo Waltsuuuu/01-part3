4de5e3ad03576afcb8b5d96d9ec3c55191746acf
Waltsuuu <waltteriheino@gmail.com>
2024-11-11 11:16:11 +0200

    commented out code for generating notes. Added code to find/fetch notes

--- mongo.js
+++ mongo.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-  `mongodb+srv://waltteriheino:${password}@fso-part3.h0fdz.mongodb.net/noteApp?retryWrites=true&w=majority&appName=FSO-part3`
+const url = `mongodb+srv://waltteriheino:${password}@fso-part3.h0fdz.mongodb.net/noteApp?retryWrites=true&w=majority&appName=FSO-part3`;
 
-mongoose.set('strictQuery',false)
+mongoose.set("strictQuery", false);
 
-mongoose.connect(url)
+mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model("Note", noteSchema);
 
+/*
 const note = new Note({
   content: 'HTML is easy',
   important: true,
@@ -30,3 +30,11 @@ note.save().then(result => {
   console.log('note saved!')
   mongoose.connection.close()
 })
+*/
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
